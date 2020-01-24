a2ff6e9e5d922aea470793b5b9c8bfa8d65d326c
Nathan Roy <nathanmroy@yahoo.com>
2020-01-23 17:42:31 -0800

    added basic structure, but failing on submit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import inflect from 'inflect';
+
+const WordList = ({ wordList }) => {
+	return (
+		<ul>
+			{wordList.map((word, idx) => {
+				return (
+					<ul key={idx}>
+						<li key={2000 + idx}>{`${word.sing} (singular)`}</li>
+						<li key={1000 + idx}>{`${word.plur} (plural)`}</li>
+						<button>X</button>
+					</ul>
+				);
+			})}
+		</ul>
+	);
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [ wordList, setWordList ] = useState([ { sing: 'bug', plur: 'bugs' } ]);
+	const [ newWord, setNewWord ] = useState('');
+
+	return (
+		<main>
+			Inflect It
+			<form
+				onSubmit={(ev) => {
+					ev.preventDefault();
+					const newEntry = {
+						sing: newWord,
+						plur: inflect.pluralize(newWord)
+					};
+					setWordList(...wordList, newEntry);
+					setNewWord('');
+				}}
+			>
+				<input type="text" value={newWord} onChange={(ev) => setNewWord(ev.target.value)} />
+				<input type="submit" disabled={!newWord} value="Submit" />
+			</form>
+			<WordList wordList={wordList} />
+		</main>
+	);
 }
 
 export default App;
